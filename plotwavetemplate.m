0b85f928dd638d1b8659500c0a278a0820515168
Najja Marshall <najja.marshall@gmail.com>
2019-02-28 13:00:34 -0500

    V1.1.4 (updated plotwavetemplate)
    - normalize each template to provide a closer look at all waveforms
    - added the option to specify the y-axis tick locations in proportions
    of the maximum template on each channel
    - added the option to specify the channel numbers used for text labels

--- plotwavetemplate.m
+++ plotwavetemplate.m
@@ -1,5 +1,5 @@
 %% PLOTWAVETEMPLATE plot waveform template
-% Plots a set of waveform templates in a grid of size channels x units
+% Plots a set of waveform templates in a grid of size channels x units.
 %
 % SYNTAX
 %   plotwavetemplate(u, varargin)
@@ -21,6 +21,13 @@
 %
 %   'yPad', <scalar>: length of padding between each row in proportion
 %       of the amplitude range of the largest waveform (default: 0.05)
+%
+%   'yTick', <numeric>: non-negative, monotonically decreasing vector that
+%       specifies the y-tick locations for the templates on each channel in
+%       units of fractions of the maximum template value on each channel
+%       (default: [2/3 1/3])
+%
+%   'chanNo', <numeric>: vector of channel numbers used for text labels
 %
 % OUTPUTS: none
 %
@@ -53,7 +60,9 @@ addRequired(P, 'u', @isnumeric)
 addParameter(P, 'cmap', 'Spectral', @ischar)
 addParameter(P, 'bright', -0.2, @(x) isscalarnum(x,-1,1))
 addParameter(P, 'xPad', 0.1, @(x) isscalarnum(x,0,Inf))
-addParameter(P, 'yPad', 0.05, @(x) isscalarnum(x,0,Inf))
+addParameter(P, 'yPad', 0.1, @(x) isscalarnum(x,0,Inf))
+addParameter(P, 'yTick', [2/3 1/3], @(x) isnumeric(x) && all(x<=1) && all(x>0) && all(diff(x)<0))
+addParameter(P, 'chanNo', 1:size(u,2), @isnumeric)
 
 % clear workspace (parser object retains the data while staying small)
 parse(P, u, varargin{:});
@@ -63,24 +72,27 @@ clear ans varargin
 
 [waveLen,nChan,nUnit] = size(u);
 
+% normalize templates
+yAbsLim = max(max(abs(u),[],1),[],3);
+u = u./yAbsLim;
+
+% horizontal and vertical offsets
+xPos = [0,cumsum(repmat((1+P.Results.xPad)*size(u,1),1,nUnit-1))];
+yPos = flipud([0;cumsum((1+P.Results.yPad)*2*ones(nChan-1,1))]);
+
+% y-tick position and values
+yTickVal = P.Results.yTick;
+yTickPos = [yPos-yTickVal, yPos, yPos+fliplr(yTickVal)];
+yTick = round((yTickPos - yPos).*yAbsLim(:));
+
+yTickPos = reshape(fliplr(yTickPos)',(1+2*length(yTickVal))*nChan,1);
+yTick = reshape(fliplr(yTick)',(1+2*length(yTickVal))*nChan,1);
+
 % color map
 cmap = brewermap(nUnit,P.Results.cmap);
 cmap = min([1 1 1],max([0 0 0], cmap + P.Results.bright));
 
-% horizontal offset
-xPos = [0,cumsum(repmat((1+P.Results.xPad)*size(u,1),1,nUnit-1))];
-
-% vertical offset
-yLim = [floor(min(u(:))), ceil(max(u(:)))];
-yPos = round(flipud([0;cumsum(repmat((1+P.Results.yPad)*diff(yLim),nChan-1,1))]));
-
-% y-tick position and values
-yTickPos = round([yPos+yLim(2)/2, yPos, yPos+yLim(1)/2]);
-yTick = yTickPos - yPos;
-
-yTickPos = reshape(yTickPos',3*nChan,1);
-yTick = reshape(yTick',3*nChan,1);
-
+% plot shapes
 figure
 hold on
 for ch = 1:nChan
@@ -100,11 +112,11 @@ set(gca,'yTickLabel',cellfun(@num2str,flipud(num2cell(yTick)),'uni',false))
 
 % channel numbers
 for ch = 1:nChan
-    text(1.01*(waveLen+xPos(end)),yPos(ch),['channel ' num2str(ch)],'fontsize',14)
+    text(1.01*(waveLen+xPos(end)),yPos(ch),['channel ' num2str(P.Results.chanNo(ch))],'fontsize',14)
 end
 
 set(gca,'fontsize',14)
 
 axis tight
 set(gca,'xlim',[0, xPos(end)+waveLen])
-set(gca,'ylim',[yLim(1), yPos(1)+yLim(2)])
+set(gca,'ylim',[-1, yPos(1)+1])
